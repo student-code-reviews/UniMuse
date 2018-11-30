a56d86f90d7c751113689f88f5a02cd6ba45ca0b
Leslie <yspark90@gmail.com>
2018-11-30 21:20:14 +0000

    Created Mixcloud module for Mixcloud search API requests.

---
+++ UniMuse/mixcloud.py
@@ -0,0 +1,43 @@
+"""Mixcloud-related functions for requests and tokens."""
+
+import requests
+import urllib
+
+
+MIXCLOUD_SEARCH_URL = "https://api.mixcloud.com/search"
+
+
+def search_data_map(response):
+    search_data = {
+        'songTitle': response['name'],
+        'albumImgURLsm': response['pictures']['thumbnail'],
+        'songURI': response['key']
+    }
+
+    return search_data
+
+
+def search(query):
+    """Mixcloud search request and response."""
+
+    if " " in query:
+        query_str = "+".join(query.split(" "))
+    else:
+        query_str = query
+
+    query_param = {
+        "q": query_str,
+        "type": "cloudcast",
+        "limit": "10",
+        "offset": "10"
+    }
+
+    url_args = "&".join(["{}={}".format(key,urllib.parse.quote(val.encode('utf-8'))) for key,val in query_param.items()])
+    query_url = f"{MIXCLOUD_SEARCH_URL}/?{url_args}"
+
+    response = requests.get(query_url).json()
+    response_lst = response['data']
+
+    search_list_data = list(map(search_data_map, response_lst))
+
+    return search_list_data
